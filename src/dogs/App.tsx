923d0c9e780ead75a288391f1fe1ffb69c59c45a
Paweł Dworniczak <dworniczakpawel@gmail.com>
2022-02-01 17:48:03 +0100

    initail commit

--- src/dogs/App.tsx
+++ src/dogs/App.tsx
@@ -3,36 +3,50 @@ import { breedsApi } from "../api";
 import { Modal } from "./Modal";
 
 function App() {
-
-  const [backgroundImage, setBackgroundImage] = useState('')
+  const [backgroundImage, setBackgroundImage] = useState("");
   const [breeds, setBreeds] = useState<string[]>([]);
   useEffect(() => {
     const run = async () => {
       try {
-        const breedImage = await breedsApi.getRandomImage()
+        const breedImage = await breedsApi.getRandomImage();
 
-        setBackgroundImage(breedImage.message)
+        setBackgroundImage(breedImage.message);
 
-        const breedListResponse = await breedsApi.getBreedList()
-        const breeds = Object.entries(breedListResponse.message).reduce<string[]>((acc, [breed, subbreeds]) => subbreeds.length ? [...acc, ...subbreeds.map((subbreed: string) => `${breed} ${subbreed}`)] : [...acc, breed], [])
-        setBreeds(breeds)
-      } catch(error) {
-        console.error(error)
+        const breedListResponse = await breedsApi.getBreedList();
+        const breeds = Object.entries(breedListResponse.message).reduce<
+          string[]
+        >(
+          (acc, [breed, subbreeds]) =>
+            subbreeds.length
+              ? [
+                  ...acc,
+                  ...subbreeds.map(
+                    (subbreed: string) => `${breed} ${subbreed}`
+                  ),
+                ]
+              : [...acc, breed],
+          []
+        );
+        setBreeds(breeds);
+      } catch (error) {
+        console.error(error);
       }
-    }
+    };
 
-    run()
-  }, [])
-  const [selectedBreed, setSelectedBreed] = useState<null|string>(null)
-  
+    run();
+  }, []);
+  const [selectedBreed, setSelectedBreed] = useState<null | string>(null);
+
   return (
-    <main style={{backgroundImage: `url(${backgroundImage})`}}>
-      <header>
-        Doggo App
-      </header>
+    <main style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <header>Doggo App</header>
       <nav>
         <ul>
-          {breeds.map((breed, index) => (<button key={index} onClick={() => setSelectedBreed(breed)}>{breed}</button>))}
+          {breeds.map((breed, index) => (
+            <button key={index} onClick={() => setSelectedBreed(breed)}>
+              {breed}
+            </button>
+          ))}
         </ul>
       </nav>
       <Modal breed={selectedBreed} close={() => setSelectedBreed(null)} />
